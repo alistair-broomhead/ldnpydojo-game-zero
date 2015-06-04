caa3ca0d125f83a0dc02fa4f920fb3c975e37743
Alistair Broomhead <alistair.broomhead@gmail.com>
2015-06-04 21:03:51 +0100

    add enemy

--- side_scroll.py
+++ side_scroll.py
@@ -4,8 +4,26 @@ WIDTH = 800
 player = Actor('ship')
 player.topleft = 10, HEIGHT/2
 
+
+class Green(Actor):
+    def __init__(self, img='happy_green_guy'):
+        super().__init__(img)
+        self.topright = WIDTH-10, HEIGHT/2
+
+    def tick(self):
+        self.x -= 2
+        self.draw()
+
+
+enemies = [Green()]
+
+
+
 def draw():
     screen.clear()
+    for enemy in enemies:
+        enemy.tick()
+        
     player.draw()
 
 def update():
@@ -18,3 +36,8 @@ def update():
         player.y = 0
     elif player.y > HEIGHT:
         player.y = HEIGHT
+
+
+
+
+
