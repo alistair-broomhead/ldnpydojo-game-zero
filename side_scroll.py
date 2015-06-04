8120599101e2e1945efdad1b244cce8e7ff40929
Alistair Broomhead <alistair.broomhead@gmail.com>
2015-06-04 20:52:04 +0100

    Make a ship move

--- side_scroll.py
+++ side_scroll.py
@@ -1,3 +1,20 @@
+HEIGHT = 600
+WIDTH = 800
+
+player = Actor('ship')
+player.topleft = 10, HEIGHT/2
+
 def draw():
     screen.clear()
-    screen.blit(images.alien, (0, 0))
+    player.draw()
+
+def update():
+    if keyboard.w :
+        player.y -= 20
+    elif keyboard.s:
+        player.y += 20
+
+    if player.y < 0:
+        player.y = 0
+    elif player.y > HEIGHT:
+        player.y = HEIGHT
