5d0e8627c894b0e516504db1460661b9031ffe1e
Alistair Broomhead <alistair.broomhead@gmail.com>
2015-06-04 21:31:11 +0100

    Make it more interesting to demo

--- side_scroll.py
+++ side_scroll.py
@@ -1,29 +1,55 @@
+import random
+
+TITLE = "Super Alien Peril"
 HEIGHT = 600
 WIDTH = 800
 
 player = Actor('ship')
 player.topleft = 10, HEIGHT/2
 
+t = 0
 
 class Green(Actor):
+
     def __init__(self, img='happy_green_guy'):
         super().__init__(img)
-        self.topright = WIDTH-10, HEIGHT/2
+        self._hash_from = object()
+        self.x = WIDTH-10
+        self.y = random.randint(0, HEIGHT)
+
+    def __hash__(self):
+        return hash(self._hash_from)
 
     def tick(self):
-        self.x -= 2
+        self.x -= 5
         self.draw()
 
 
-enemies = [Green()]
-
+enemies = {Green()}
+projectiles = []
 
 
 def draw():
+    global t
+
+    t += 1
+
+    if t % 50 == 0:
+        enemies.add(Green())
+
     screen.clear()
+
+    to_remove = set()
+
     for enemy in enemies:
         enemy.tick()
-        
+
+        if enemy.x < 10:
+            to_remove.add(enemy)
+
+    for enemy in to_remove:
+        enemies.remove(enemy)
+
     player.draw()
 
 def update():
